f634f80163229c0b6fbc062aa08eb25113f15f39
Gio Cornejo <cornejog553@gmail.com>
2023-10-24 22:15:08 -0400

    Finished up the UI

--- CV-Application/src/components/Resume.jsx
+++ CV-Application/src/components/Resume.jsx
@@ -11,6 +11,37 @@ function Resume(){
                     <p>Address</p>
                 </div>
             </div> 
+            <div className='educationAndExperienceSection'>
+                <div className='education'>
+                    <h2>Education</h2>
+                </div>
+                <div className='education-Content'>
+                    <div>
+                        <p>08/2020 – present</p>
+                        <p>New York City, US</p>
+                    </div>
+                    <div className='universityAndDegree'>
+                        <p className='university'>London City University</p>
+                        <p>Bachelors in Economics</p>
+                    </div>
+                </div>
+
+                <div className='experience'>
+                    <h2>Experience</h2>
+                </div>
+                <div className='experience-Content'>
+                    <div>
+                        <p>08/2020 – present</p>
+                        <p>New York City, US</p>
+                    </div>
+                    <div className='universityAndDegree'>
+                        <p className='jobCompany'>Umbrella Inc.</p>
+                        <p>UX & UI Designer</p>
+                        <p>Designed and prototyped user interface patterns for various clients in various industries, ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android</p>
+                    </div>
+                </div>
+            </div>
+            
             
         </div>
     )
